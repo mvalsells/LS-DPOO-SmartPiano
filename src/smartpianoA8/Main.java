80dbda80206aa09a5b8960e7f051df2dfe525260
christianhasko <christian.hasko@students.salle.url.edu>
2021-04-06 11:28:11 +0200

    MVC
    Took 46 seconds

---
+++ src/smartpianoA8/Main.java
@@ -0,0 +1,12 @@
+package smartpianoA8;
+
+import smartpianoA8.Persistence.*;
+import smartpianoA8.Business.*;
+import smartpianoA8.Presentation.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SmartPiano-A8");
+
+    }
+}
