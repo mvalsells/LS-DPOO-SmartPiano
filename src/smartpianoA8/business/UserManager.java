8594b81c0fffbf3d75e356de8ab3ed518734b323
Marc Valsells <marc@valsells.me>
2021-04-14 16:31:57 +0200

    Afegir metodes a UserManager

--- src/smartpianoA8/business/UserManager.java
+++ src/smartpianoA8/business/UserManager.java
@@ -4,6 +4,10 @@ import smartpianoA8.business.entity.User;
 import smartpianoA8.persistence.dao.UserDAO;
 import smartpianoA8.persistence.dao.sql.SQLUserDAO;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class UserManager {
     //Constructor
     public UserManager(){
@@ -28,6 +32,44 @@ public class UserManager {
 
     public void removeUser(User user){
         UserDAO usrTmp = new SQLUserDAO();
-        usrTmp.removeUser(user);
+        if (usrTmp.getUserByUsername(user.getUserName()) != null) {
+            usrTmp.removeUser(user);
+        }
+    }
+
+    public boolean modifyUserEmail(User user, String newEmail){
+        UserDAO usrTmp = new SQLUserDAO();
+        if (usrTmp.getUserByUsername(user.getUserName()) != null){
+            //TODO Falta implementació a la interifcie UsuariDAO
+        }
+        return true;
+    }
+
+    public boolean modifyUserPassword(User user, String newPasswordHash){
+        UserDAO usrTmp = new SQLUserDAO();
+        if (usrTmp.getUserByUsername(user.getUserName()) != null){
+            //TODO Falta implementació a la interifcie UsuariDAO
+        }
+        return true;
+    }
+    public boolean modifyUserUserName(User user, String newUsername){
+        UserDAO usrTmp = new SQLUserDAO();
+        if (usrTmp.getUserByUsername(user.getUserName()) != null){
+            //TODO Falta implementació a la interifcie UsuariDAO
+        }
+        return true;
+    }
+
+    protected String encryptPassword(String plainTextPassword){
+        //TODO Acabar de mirar bé
+        // https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] hash = digest.digest(plainTextPassword.getBytes(StandardCharsets.UTF_8));
+        return "Hello";
     }
 }
