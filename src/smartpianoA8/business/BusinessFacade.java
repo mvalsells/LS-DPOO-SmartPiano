f27d868b38e5f8dc48d59a374b8e408b20822114
christianhasko <christian.hasko@students.salle.url.edu>
2021-04-14 08:39:25 +0200

    bussinesfacade updated
    Took 16 minutes

--- src/smartpianoA8/business/BusinessFacade.java
+++ src/smartpianoA8/business/BusinessFacade.java
@@ -1,4 +1,20 @@
 package smartpianoA8.business;
 
+import smartpianoA8.business.entity.Song;
+import smartpianoA8.business.entity.User;
+
 public interface BusinessFacade {
+
+    public void addSong(String nom, String autor, String duracio, String time, String directori, Boolean isPublic, String nomUsuari);
+    public void removeSong(Song song);
+    public boolean createUser(String userName, String email, String password, String type);
+    public void  removeUser(String currentUser);
+    public User getUserByEmail(String email);
+    public User getUserByUsername(String username);
+    public boolean newPlayList(String nom, String nomUsuari);
+    public void removePlayList(int idPlayList);
+    public boolean modifyUserEmail(String currentEmail, String newEmail);
+    public boolean modifyPassword(String currentName, String newPassword);
+    public boolean modifyUserName(String currentUserName, String newUserName);
+
 }
