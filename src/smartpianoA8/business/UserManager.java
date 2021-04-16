6ef13ca6fab97830f1db88a88385fe1ad944cc65
christianhasko <christian.hasko@students.salle.url.edu>
2021-04-16 17:16:51 +0200

    Login
    Took 3 minutes

--- src/smartpianoA8/business/UserManager.java
+++ src/smartpianoA8/business/UserManager.java
@@ -24,6 +24,7 @@ public class UserManager {
 
     //TODO Hablar con diseño de vistas para en lugar de crear funciones para cambiar usuario, contraseña, email... Una SOLA funcion a la que mandamos un string con QUE cambiar y el que así nos ahorramos muchas funciones y simplificamos codigo.
 
+
     public void registerUser (String username, String email, String password, String type) throws PasswordException, UserManagerException {
 
         //Check user data
