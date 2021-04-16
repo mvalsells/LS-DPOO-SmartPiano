fedfbace829fc4c9b3a04c99dc8c4fd1355aa851
Marc Valsells <marc@valsells.me>
2021-04-16 17:14:09 +0200

    Prova tonta, un comentari

--- src/smartpianoA8/business/UserManager.java
+++ src/smartpianoA8/business/UserManager.java
@@ -12,6 +12,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class UserManager {
+
+    //Prova Christian
+
     //Atributs
     private UserDAO userDAO;
     //Constructor
