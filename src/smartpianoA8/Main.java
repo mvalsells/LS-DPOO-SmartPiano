ef791ba08d5b176d6d7a3cacc278f76e4ef33e75
christianhasko <christian.hasko@students.salle.url.edu>
2021-05-03 10:50:11 +0200

    HTML Scrapping empezado IMPORTANTE: añadir libreria al proyecto
    Took 35 minutes

--- src/smartpianoA8/Main.java
+++ src/smartpianoA8/Main.java
@@ -3,6 +3,7 @@ package smartpianoA8;
 import smartpianoA8.business.BusinessFacade;
 
 import smartpianoA8.business.BusinessFacadeImpl;
+import smartpianoA8.business.HtmlScrapping;
 import smartpianoA8.business.entity.User;
 import smartpianoA8.business.exceptions.PasswordException;
 import smartpianoA8.business.exceptions.UserManagerException;
@@ -14,19 +15,21 @@ import smartpianoA8.persistence.dao.sql.SQLUserDAO;
 import smartpianoA8.presentation.views.PianoView;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         final int ERROR_CODE_FILE = 1;
         //BBDD v
         //rebre dades fitxer
 
 
+        HtmlScrapping htmlScrapping = new HtmlScrapping();
+        htmlScrapping.Scrapping();
 
-
-        JsonReadable jsonReader = new JsonReader();
+        /*JsonReadable jsonReader = new JsonReader();
 
         try {
             jsonReader.readJsonConfig();
@@ -95,7 +98,6 @@ public class Main {
         ///registerView.setSize(400,400);
         /*smartpianoA8.presentation.views.RegisterView registerView = new smartpianoA8.presentation.views.RegisterView();
         registerView.setVisible(true);*/
-
 
 
     }
---
+++ src/smartpianoA8/business/HtmlScrapping.java
@@ -0,0 +1,73 @@
+package smartpianoA8.business;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import smartpianoA8.persistence.dao.UserDAO;
+
+import java.io.IOException;
+
+public class HtmlScrapping {
+
+    public static final String url1 = "https://www.mutopiaproject.org/cgibin/make-table.cgi?startat=";
+    public static int page = 0;
+    public static final String url2 = "&Instrument=Piano";
+
+    public HtmlScrapping(/*UserDAO userDAO*/){
+    }
+
+    public void Scrapping() throws IOException {
+
+        page=0;
+        System.out.println(url1.concat(page+url2));
+
+        if(getConnectionStatus(url1.concat(page+url2)) == 200) {
+            Document document = getHtmlDocument(url1.concat(page+url2));
+            Elements entry = document.select("table-bordered result-table");
+            System.out.println("lele");
+        }
+
+    }
+
+
+    /**
+     * Compruebo el Status code de la respuesta que recibo al hacer la petición
+     * 		200 OK			        300 Multiple Choices
+     * 		301 Moved Permanently	305 Use Proxy
+     * 		400 Bad Request		    403 Forbidden
+     * 		404 Not Found		    500 Internal Server Error
+     * 		502 Bad Gateway		    503 Service Unavailable
+     * @param url
+     * @return Status Code
+     */
+    private static int getConnectionStatus(String url) throws IOException {
+        Connection.Response response = null;
+
+        try {
+            response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
+        } catch (IOException e) {
+            System.err.println("Exception in obtainig status code: " + e.getMessage());
+        }
+        return response.statusCode();
+    }
+
+    /**
+     * Devuelvo un objeto de la clase Document con el contenido del
+     * HTML de la web que me permitirá parsearlo con los métodos de la librelia JSoup
+     * @param url
+     * @return Documento con el HTML
+     */
+    private static Document getHtmlDocument(String url) {
+
+        Document document = null;
+        try {
+            document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+        } catch (IOException ex) {
+            System.out.println("Exception in obtaining HTML from page" + ex.getMessage());
+        }
+        return document;
+    }
+
+}
