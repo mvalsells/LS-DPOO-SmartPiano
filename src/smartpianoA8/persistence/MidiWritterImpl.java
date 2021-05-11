c19b22bcbc5b6b3279817a56c893cd845595484b
christianhasko <christian.hasko@students.salle.url.edu>
2021-05-11 14:17:43 +0200

    recording doneeeeEEEEEEE :) falta mirar controllers y hacer segun MVC
    Took 1 hour 56 minutes

--- src/smartpianoA8/persistence/MidiWritterImpl.java
+++ src/smartpianoA8/persistence/MidiWritterImpl.java
@@ -1,32 +1,74 @@
 package smartpianoA8.persistence;
 
+import smartpianoA8.business.entity.Song;
 import smartpianoA8.presentation.views.Key;
 
 import javax.sound.midi.*;
+import java.io.File;
+import java.io.IOException;
 
 public class MidiWritterImpl implements MidiWritter {
 
-    Sequence sequence;
+    Sequence sequence = null;
     Track pianoTrack;
     Sequencer sequencer;
+    private boolean recording = false;
+    private long startTime = 0;
+
 
     public MidiWritterImpl() {
+    }
+
+    public void startRecording() {
         try {
-            sequence = new Sequence(Sequence.PPQ, 24);
-            sequencer = MidiSystem.getSequencer();
+
+            sequence = new Sequence(Sequence.PPQ, 500);
+
+
+
+            //sequencer = MidiSystem.getSequencer();
             pianoTrack = sequence.createTrack();
+            //pianoTrack.add(createMidiEvent(ShortMessage.PROGRAM_CHANGE, 0,0,0,0));
+            //sequencer.open();
+            //sequencer.setSequence(sequence);
+            sequencer = MidiSystem.getSequencer();
+            sequencer.setSequence(sequence);
+            sequencer.open();
+            sequencer.recordEnable(pianoTrack, 0);
             sequencer.startRecording();
+            System.out.println("Started recording...");
+            recording = true;
+            startTime = System.currentTimeMillis();
         } catch (InvalidMidiDataException | MidiUnavailableException e) {
             e.printStackTrace();
         }
     }
 
-    public void setOnMessage(Key key) {
-        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_ON, 0, key.getNote(), 127,sequencer.getTickPosition() * (sequence.getResolution() / 500)));
+    public void endRecording() {
+        recording = false;
+        sequencer.stopRecording();
+        try {
+            Sequencer sequencerAux = MidiSystem.getSequencer();
+            sequencerAux.open();
+            sequencerAux.setSequence(sequence);
+            sequencerAux.start();
+        } catch (MidiUnavailableException | InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setOffMessage(Key key) {
-        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_OFF, 0, key.getNote(), 0, sequencer.getTickPosition() * (sequence.getResolution() / 500)));
+    public boolean getIsRecording() {
+        return recording;
+    }
+
+    public void setOnMessage(int key, long startedNote) {
+        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_ON, 0, key, 127,/*System.currentTimeMillis()-startTime*/startedNote-startTime));
+        System.out.println("Key: " + key + " ONNN");
+    }
+
+    public void setOffMessage(int key, long endedNote) {
+        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_OFF, 0, key, 127, endedNote-startTime));
+        System.out.println("Key: " + key + " OFFF");
     }
 
     private static MidiEvent createMidiEvent(int command, int channel, int data1, int data2, long instant) {
