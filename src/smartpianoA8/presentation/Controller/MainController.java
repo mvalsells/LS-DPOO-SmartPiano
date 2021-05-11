53146662b8c8bde13dfe1e150c88c1c6a58f5eca
pausantacreu <pausantacreu@users.noreply.github.com>
2021-05-11 12:10:43 +0200

    action listener record
    Took 13 minutes

--- src/smartpianoA8/presentation/Controller/MainController.java
+++ src/smartpianoA8/presentation/Controller/MainController.java
@@ -1,6 +1,7 @@
 package smartpianoA8.presentation.Controller;
 
 import smartpianoA8.presentation.views.*;
+import smartpianoA8.presentation.views.customComponents.JPPiano;
 
 import javax.sound.midi.MidiChannel;
 import java.awt.event.*;
@@ -38,6 +39,9 @@ public class MainController implements ActionListener , KeyListener,MouseListene
                 break;
             case MainView.chgToFavs:
                 mainFrame.changePanel(MainView.chgToFavs);
+                break;
+            case JPPiano.startRecording:
+                //Christian aqui tu action listener
                 break;
         }
 
