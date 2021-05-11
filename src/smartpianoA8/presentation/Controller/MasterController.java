809acf13c142469228e37390cbc73611743c909d
christianhasko <christian.hasko@students.salle.url.edu>
2021-05-11 10:39:01 +0200

    write midi files start
    Took 2 minutes

--- src/smartpianoA8/presentation/Controller/MasterController.java
+++ src/smartpianoA8/presentation/Controller/MasterController.java
@@ -63,10 +63,6 @@ public class MasterController {
         mainController = new MainController(mainFrame,midiChannel);
     }
 
-    public int getResolutionFromOriginalSong() {
-        return businessFacade.getTrackResolution();
-    }
-
     public void registerAllControlers(){
         //Register this controller to other controllers
         wellcomeController.registerController(this);
