c64ce2b44b95f954ad4c4b429c36ec5312e78786
christianhasko <christian.hasko@students.salle.url.edu>
2021-05-11 11:01:32 +0200

    write midi files updated
    Took 11 minutes

--- src/smartpianoA8/persistence/MidiWritterImpl.java
+++ src/smartpianoA8/persistence/MidiWritterImpl.java
@@ -8,14 +8,38 @@ public class MidiWritterImpl implements MidiWritter {
 
     Sequence sequence;
     Track pianoTrack;
+    Sequencer sequencer;
 
-    public MidiWritterImpl(int resolution) {
+    public MidiWritterImpl() {
         try {
-            sequence = new Sequence(Sequence.PPQ, resolution);
-
-        } catch (InvalidMidiDataException e) {
+            sequence = new Sequence(Sequence.PPQ, 24);
+            sequencer = MidiSystem.getSequencer();
+            pianoTrack = sequence.createTrack();
+            sequencer.startRecording();
+        } catch (InvalidMidiDataException | MidiUnavailableException e) {
             e.printStackTrace();
         }
+    }
+
+    public void setOnMessage(Key key) {
+        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_ON, 0, key.getNote(), 127,sequencer.getTickPosition() * (sequence.getResolution() / 500)));
+    }
+
+    public void setOffMessage(Key key) {
+        pianoTrack.add(createMidiEvent(ShortMessage.NOTE_OFF, 0, key.getNote(), 0, sequencer.getTickPosition() * (sequence.getResolution() / 500)));
+    }
+
+    private static MidiEvent createMidiEvent(int command, int channel, int data1, int data2, long instant) {
+        ShortMessage shortMessage = new ShortMessage();
+        try {
+            shortMessage.setMessage(
+                    command,
+                    channel,
+                    data1,
+                    data2);
+        } catch (InvalidMidiDataException e) {
+        }
+        return new MidiEvent(shortMessage, instant);
     }
 
 
