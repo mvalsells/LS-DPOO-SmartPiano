149a2f9d1a0f21763bf642ef19ffbfb12045412a
Albert Garangou Culebras <albertgarangou@gmail.com>
2021-05-12 17:01:14 +0200

    back code notes 2
    Temps: 16 minutes

---
+++ src/smartpianoA8/presentation/Controller/PianoCascadeController.java
@@ -0,0 +1,22 @@
+package smartpianoA8.presentation.Controller;
+
+import smartpianoA8.presentation.views.CascadePanel;
+
+import javax.swing.*;
+
+public class PianoCascadeController extends JPanel implements  Runnable{
+    private CascadePanel cascada;
+
+    public PianoCascadeController(){
+
+
+
+
+
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
