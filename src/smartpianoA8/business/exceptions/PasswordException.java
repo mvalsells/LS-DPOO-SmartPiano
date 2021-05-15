a4ff48f347b0f264a996e5700086ffdfd8778e01
Albert Garangou Culebras <albertgarangou@gmail.com>
2021-05-15 14:04:38 +0200

    javadoc
    Temps: 14 minutes

--- src/smartpianoA8/business/exceptions/PasswordException.java
+++ src/smartpianoA8/business/exceptions/PasswordException.java
@@ -1,5 +1,11 @@
 package smartpianoA8.business.exceptions;
 
+/**
+ * Exepció per contrassenyes d'usuaris
+ * @see smartpianoA8.business.entity.User
+ * @version 1.0
+ * @author Albert Clarimont, Christian Hasko, Marc Valsells i Albert Garangou
+ */
 public class PasswordException extends Exception{
     //Atributs de la exception
     boolean passwordToShort;
@@ -10,6 +16,16 @@ public class PasswordException extends Exception{
     boolean hasNotNumber;
     boolean passwordsDifferent;
 
+    /**
+     * Constructor
+     * @param passwordToShort boolean contrassenya massa curta
+     * @param passwordsDifferent boolean contrassenya incorrecta
+     * @param equalsEmail boolean contrassenya = email
+     * @param equalsUsername boolean contrassenya = user
+     * @param hasNotUpperCase contrassenya no té majúscules
+     * @param hasNotLowerCase contrassenya no té minúscules
+     * @param hasNotNumber contrassenya no té nombres
+     */
     public PasswordException(boolean passwordToShort, boolean passwordsDifferent, boolean equalsEmail, boolean equalsUsername, boolean hasNotUpperCase, boolean hasNotLowerCase, boolean hasNotNumber) {
         super();
         this.passwordToShort = passwordToShort;
@@ -22,34 +38,67 @@ public class PasswordException extends Exception{
      //   toString();
     }
 
+    /**
+     * Getter estat contrassenya curta
+     * @return boolean true/false estat
+     */
     public boolean isPasswordToShort() {
         return passwordToShort;
     }
 
+    /**
+     * Getter estat contrassenya = email
+     * @return true/false estat
+     */
     public boolean isEqualsEmail() {
         return equalsEmail;
     }
 
+    /**
+     * Getter estat = user
+     * @return true/false estat
+     */
     public boolean isEqualsUsername() {
         return equalsUsername;
     }
 
+    /**
+     * Getter estat contrassenya no ABC
+     * @return true/fasle estat
+     */
     public boolean isHasNotUpperCase() {
         return hasNotUpperCase;
     }
 
+    /**
+     * Getter estat contrassenya no abc
+     * @return estat true/false
+     */
     public boolean isHasNotLowerCase() {
         return hasNotLowerCase;
     }
 
+    /**
+     * Getter estat no nombres
+     * @return true/false estat
+     */
     public boolean isHasNotNumber() {
         return hasNotNumber;
     }
 
+    /**
+     * Getter contrassenya incorrecta
+     * @return true/false estat
+     */
     public boolean isPasswordsDifferent() {
         return passwordsDifferent;
     }
 
+    /**
+     * Mètode que converteix i retorna tots els estats en String
+     * @return String estats
+     * @implNote per debugging només
+     */
     @Override
     public String toString() {
         return "PasswordException{" +
--- src/smartpianoA8/business/exceptions/UserManagerException.java
+++ src/smartpianoA8/business/exceptions/UserManagerException.java
@@ -1,5 +1,11 @@
 package smartpianoA8.business.exceptions;
 
+/**
+ * Exepció per noms d'usuari i email
+ * @see smartpianoA8.business.entity.User
+ * @version 1.0
+ * @author Albert Clarimont, Christian Hasko, Marc Valsells i Albert Garangou
+ */
 public class UserManagerException extends Exception{
     boolean usernameExists;
     boolean emailExists;
@@ -7,6 +13,15 @@ public class UserManagerException extends Exception{
     boolean passwordIncorrect;
     boolean emailIncorrect;
 
+    /**
+     * Constructor amb els possibles estats
+     * @param usernameExists boolean usuari existent
+     * @param emailExists boolean email existent
+     * @param emailIncorrect boolean email incorrecte
+     * @param typeIncorrect boolean tipus de compte incorrecte
+     * @param passwordIncorrect boolean contra incorrecta
+     * @see PasswordException per <passwordIncorrect></passwordIncorrect>
+     */
     public UserManagerException(boolean usernameExists, boolean emailExists, boolean emailIncorrect, boolean typeIncorrect, boolean passwordIncorrect) {
         this.usernameExists = usernameExists;
         this.emailExists = emailExists;
@@ -15,22 +30,43 @@ public class UserManagerException extends Exception{
         this.passwordIncorrect = passwordIncorrect;
     }
 
+    /**
+     * Getter de l'estat d'usuari existent
+     * @return true/false de l'estat
+     */
     public boolean isUsernameExists() {
         return usernameExists;
     }
 
+    /**
+     * Getter de l'estat d'email existent
+     * @return true/false de l'estat
+     */
     public boolean isEmailExists() {
         return emailExists;
     }
 
+    /**
+     * Getter de l'estat de tipus incorrecte de compte
+     * @return true/false de l'estat
+     */
     public boolean isTypeIncorrect() {
         return typeIncorrect;
     }
 
+    /**
+     * Getter de l'estat de contrassenya incorrecte
+     * @return true/false de l'estat
+     */
     public boolean isPasswordIncorrect() {
         return passwordIncorrect;
     }
 
+    /**
+     * Mètode per convertir a String els estats
+     * @implNote per debugging només
+     * @return String amb els estats
+     */
     @Override
     public String toString() {
         return "UserManagerException{" +
