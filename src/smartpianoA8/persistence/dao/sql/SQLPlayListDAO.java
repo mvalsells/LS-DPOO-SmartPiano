c0be4e2f3c866bc033656f79962a54c57d7b608b
Albert Garangou Culebras <albertgarangou@gmail.com>
2021-05-19 19:28:54 +0200

    playlist emplenar cançons HOSTA next
    Temps: 23 minutes

--- src/smartpianoA8/persistence/dao/sql/SQLPlayListDAO.java
+++ src/smartpianoA8/persistence/dao/sql/SQLPlayListDAO.java
@@ -48,7 +48,7 @@ public class SQLPlayListDAO implements PlayListDAO{
 
 
 
-        String query = "SELECT ";
+        //String query = "SELECT ";
 
         String query = "INSERT INTO SongPlaylist(idSong, IDPlayList) VALUES ('" +
                 song.getIdSong() + "', '" + playList.getIdPlayList() + "');";
@@ -138,19 +138,22 @@ public class SQLPlayListDAO implements PlayListDAO{
     @Override
     public ArrayList<Song> getPlayListSongs(PlayList playList) {
         ArrayList<Song> songs = new ArrayList<>();
-        String query = "SELECT idSong FROM SongPlaylist WHERE IDPlayList = " + playList.getIdPlayList() + ";";
+        String query = "SELECT idSong FROM SongPlaylist WHERE IdPlayList = " + playList.getIdPlayList() + ";";
         ResultSet result = connector.selectQuery(query);
-        String query2;
 
+        String query2;
         try{
             while(result.next()) {
-                query2 = "SELECT IDSong, NumReproduccions, Nom, Autor, Duracio, DataEnregistrament, Directori, isPublic, NomUsuari FROM Song WHERE IDSong = " + result.getInt("idSong") + ";";
+                query2 = "SELECT * FROM Song WHERE idSong = " + result.getInt("idSong") + ";";
                 ResultSet result2 = connector.selectQuery(query2);
+                result2.next();
+                //System.out.println(result2.getInt("idSong"));
                 songs.add(new Song(result2.getInt("idSong"), result2.getFloat("Duracio"), result2.getString("Nom"), result2.getString("Autor"), result2.getString("DataEnregistrament"), result2.getString("Directori"), result2.getInt("isPublic"), result2.getString("NomUsuari"), result2.getString("Midi")));
             }
+            return songs;
         }catch (SQLException e){
             e.printStackTrace();
-            return songs;
+
 
         }
         return null;
