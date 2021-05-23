b9c6d12174023a9ac32f2f007c55aa4f09d3677c
Albert Garangou Culebras <albert.garangou@students.salle.url.edu>
2021-05-23 13:27:27 +0200

    javadoc

--- src/smartpianoA8/presentation/views/customComponents/ColorScheme.java
+++ src/smartpianoA8/presentation/views/customComponents/ColorScheme.java
@@ -1,7 +1,11 @@
 package smartpianoA8.presentation.views.customComponents;
 
 import java.awt.*;
-
+/**
+ * Classe que recull els colors més utilitzats per canviar la paleta de colors de forma centrelitzada
+ * @version 1.0
+ * @author Pau Santacreu, Albert Clarimont, Marc Valsells, Christian Hasko i Albert Garangou
+ */
 public class ColorScheme {
 
     //Main View
@@ -15,6 +19,5 @@ public class ColorScheme {
     public static final Color ORANGE_START = new Color(249,171,15);
     public static final Color BlueButton =new Color(116,148,223);
     public static final Color DarkButton =new Color(159,159,159);
-
     public static final Color DARK_GREEN = new Color(0,102,0);
 }
--- src/smartpianoA8/presentation/views/customComponents/GraficsStatisticsDreta.java
+++ src/smartpianoA8/presentation/views/customComponents/GraficsStatisticsDreta.java
@@ -6,15 +6,32 @@ import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Classe per la generació de gràfiques pel nombre de reproduccions, utilitzant Graphics i Graphics2D insertat en un JPanel que es pot unir
+ * com a "embeded" a diferents vistes
+ * @implNote No s'utilitza una herència per la sensillesa que suposaria la classe abstracte mare "GraficsStatistics"
+ * @version 1.0
+ * @author Pau Santacreu, Albert Clarimont, Marc Valsells, Christian Hasko i Albert Garangou
+ */
 public class GraficsStatisticsDreta extends JPanel {
     private final Color color;
     private final ArrayList<Integer> reproduccions;
 
+    /**
+     * Constructor amb l'Array de dades de l'eix X i el color a utilitzar (modificable)
+     * @param reproduccions ArrayList d'enters amb les variables Y de cada X de reproduccions
+     * @param color color a utilitzar per la línia del gràfic
+     */
     public GraficsStatisticsDreta(ArrayList<Integer> reproduccions, Color color){
         this.color = color;
         this.reproduccions = reproduccions;
     }
 
+    /**
+     * Mètode per printar les línies de gràfics sobre un JPanel, de X [0,23] i Y [0, infinit]
+     * @param grafics objecte que s'utilitza per renderitzar els diferents objectes afegits sobre la vista bàsica
+     * @see Graphics
+     */
     @Override
     protected void paintComponent(Graphics grafics){
         super.paintComponent(grafics);
@@ -65,14 +82,19 @@ public class GraficsStatisticsDreta extends JPanel {
 
     }
 
+    /**
+     * Mètode per printar un text rotat respecte la seva cantonada suiperior esquerra
+     * @param g2d objecte Graphics2D que renderitza el text i proporciona les eines
+     * @param x double posició X del text
+     * @param y double posició Y del text
+     * @param text String text a rotar
+     */
     private void drawRotate(Graphics2D g2d, double x, double y, String text)
     {
         g2d.translate((float)x,(float)y);
-        g2d.rotate(Math.toRadians(45));
+        g2d.rotate(Math.toRadians(45));//sempre es rota a 45º
         g2d.drawString(text,0,0);
-        g2d.rotate(-Math.toRadians(45));
+        g2d.rotate(-Math.toRadians(45));//sempre es rota a 45º
         g2d.translate(-(float)x,-(float)y);
     }
-
-
 }
