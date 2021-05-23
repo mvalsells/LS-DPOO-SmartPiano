28149143c5907d39f16eced4c382ff206dd24468
Albert Garangou Culebras <albertgarangou@gmail.com>
2021-05-23 22:32:11 +0200

    javadoc
    Temps: 57 seconds

--- src/smartpianoA8/persistence/HashMapFile.java
+++ src/smartpianoA8/persistence/HashMapFile.java
@@ -3,7 +3,11 @@ package smartpianoA8.persistence;
 import smartpianoA8.presentation.views.customComponents.Tecla;
 
 import java.util.HashMap;
-
+/**
+ * Classe per
+ * @version 1.0
+ * @author Pau Santacreu, Albert Clarimont, Marc Valsells, Christian Hasko i Albert Garangou
+ */
 public interface HashMapFile {
     void write(HashMap<Integer, Tecla> hmTeclas, String username);
     HashMap<Integer, Tecla> read(String username);
