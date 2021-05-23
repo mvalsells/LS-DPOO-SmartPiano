fae684cb02ae4e7f225cc72945b7217050027b2c
Albert Garangou Culebras <albert.garangou@students.salle.url.edu>
2021-05-23 12:50:46 +0200

    javadoc

--- src/smartpianoA8/presentation/views/customComponents/BlackKey.java
+++ src/smartpianoA8/presentation/views/customComponents/BlackKey.java
@@ -2,11 +2,22 @@ package smartpianoA8.presentation.views.customComponents;
 
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * Calsse que representa una tecla negra del piano en vistes
+ * @version 1.0
+ * @author Pau Santacreu, Albert Clarimont, Marc Valsells, Christian Hasko i Albert Garangou
+ * @see Key
+ * @see JButton
+ */
 public class BlackKey extends JButton implements Key {
 
     final int note;
     static int BWD = (int) (WD *1.2);
+
+    /**
+     * Constructor amb el paràmetre de posicionament al teclat
+     * @param pos Int posició del teclat de la nota relativa del teclat
+     */
     public BlackKey(int pos) {
         note = baseNote + 1 + 2 * pos + (pos + 3) / 5 + pos / 5;
         int left = 10 + BWD
@@ -23,11 +34,22 @@ public class BlackKey extends JButton implements Key {
 
     }
 
+    /**
+     * Mèotde per obtenir el codi de la nota
+     * @return int codi absolut entre totes les octaves
+     */
+    @Override
     public int getNote () {
         return note;
     }
 
-
+    /**
+     * Mètode per pintar una nota quan és clicada
+     */
     public void clicarNota(){setBackground(Color.RED); setBorder(BorderFactory.createLineBorder(Color.RED,11));}
+
+    /**
+     * Mèotde per despintar una nota i tornar-la al color incial
+     */
     public void desclicarNota(){setBackground(Color.BLACK);setBorder(BorderFactory.createLineBorder(Color.BLACK,11));}
 }
