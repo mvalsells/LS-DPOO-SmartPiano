7b35b45c468b7cb8e0b7be012ee3e72623b5a653
christianhasko <christian.hasko@students.salle.url.edu>
2021-05-23 19:06:01 +0200

    file deletor. ADD COMMONS-IO to project libraries
    Took 6 minutes

--- src/smartpianoA8/persistence/FileDeletorImpl.java
+++ src/smartpianoA8/persistence/FileDeletorImpl.java
@@ -1,4 +1,21 @@
 package smartpianoA8.persistence;
 
+import org.apache.commons.io.FileUtils;
+import smartpianoA8.business.entity.User;
+
+import java.io.File;
+import java.io.IOException;
+
 public class FileDeletorImpl implements FileDeletor {
+
+    public FileDeletorImpl() {}
+
+    public void removeUserSongsWhenDeletedUser(User user) {
+        try {
+            FileUtils.deleteDirectory(new File("resources/midiFiles/"+user.getUsername()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
